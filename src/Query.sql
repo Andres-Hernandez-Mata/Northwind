5b71f83dee191bfa053cb8d3e550c21b01d106f3
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-08-17 20:24:56 -0500

    Actualizar 9

--- src/Query.sql
+++ src/Query.sql
@@ -3,25 +3,30 @@ Use Northwind;
 SELECT * FROM 
 dbo.Customers 
 WHERE Country IN ('USA');
+
 /* 2. Seleccionar productos cuya existencia esté entre 15 y 39 piezas */
 SELECT * FROM 
 dbo.Products 
 WHERE UnitsInStock >= 15 AND UnitsInStock <= 39
 ORDER BY UnitsInStock;
+
 /* 3. Seleccione los proveedores cuyo cargo de contacto es gerente de marketing */
 SELECT * FROM 
 dbo.Suppliers 
 WHERE ContactTitle LIKE '%Marketing%';
+
 /* 4. Seleccione los pedidos cuya fecha de venta fue entre el 27 de enero de 1997 y el 24 de marzo de 1997 */
 SELECT * FROM 
 dbo.Orders 
 WHERE OrderDate >= ('1997-01-27 00:00:00.000') AND OrderDate <= ('1997-03-24 00:00:00.000');
+
 /* 5. Seleccione el nombre del producto, el precio unitario y las cantidades en stock de todos los productos que se han vendido en EE. UU */
 SELECT P.ProductName, P.UnitPrice, P.UnitsInStock
 FROM dbo.Products P, dbo.[Order Details] OD, dbo.Orders O 
 WHERE (OD.ProductID = P.ProductID) AND
 (O.OrderID = OD.OrderID) AND
 O.ShipCountry = 'USA';
+
 /* 6. Seleccione los productos que pertenecen al proveedor New Orleans Cajun Delights */ 
 SELECT * FROM 
 dbo.Products P
@@ -38,6 +43,7 @@ WHERE OD.OrderID = 10248;
 SELECT * FROM dbo.Orders O
 INNER JOIN dbo.Customers C ON (C.CustomerID = O.CustomerID)
 WHERE C.CompanyName = 'Hanari Carnes';
+
 /* 9. Seleccione el empleado, la categoría de producto y el producto de todos los empleados que procesaron el pedido en 1997 */
 SELECT CONCAT (E.FirstName, ' ', E.LastName) Employee, 
 C.CategoryName, P.ProductName FROM 
@@ -46,11 +52,13 @@ INNER JOIN dbo.Employees E ON (E.EmployeeID = O.EmployeeID)
 INNER JOIN dbo.[Order Details] OD ON (OD.OrderID = O.OrderID)
 INNER JOIN dbo.Products P ON (P.ProductID = OD.ProductID)
 INNER JOIN dbo.Categories C ON (C.CategoryID = P.CategoryID)
-WHERE O.OrderDate = '1997-01-01 00:00:00.000';
+WHERE O.OrderDate BETWEEN '1997-01-01 00:00:00.000' AND '1997-12-31 00:00:00.000';
+
 /* 10. Seleccione clientes cuyo país sea EE. UU., ESPAÑA, ITALIA */
 SELECT * FROM dbo.Customers C
 WHERE C.Country IN ('USA', 'SPAIN', 'ITALY')
 ORDER BY C.Country;
+
 /* 11. Seleccione el producto mas caro y mas barato */
 SELECT * FROM dbo.Products P
 WHERE P.UnitPrice 
@@ -58,13 +66,15 @@ IN (SELECT MAX(P.UnitPrice) FROM dbo.Products P)
 UNION 
 SELECT * FROM dbo.Products P
 WHERE P.UnitPrice 
-IN (SELECT MIN(P.UnitPrice) FROM dbo.Products P)
+IN (SELECT MIN(P.UnitPrice) FROM dbo.Products P);
+
 /* 12. Seleccione el pedido de envio mas barato, el envio medio y el envio mas caro */
 SELECT MIN(OS.Subtotal) ExpensiveShipping, 
 MAX(OS.Subtotal) CheapestShipping, 
 AVG(OS.Subtotal) AverageShipping
 FROM dbo.Orders O
-INNER JOIN dbo.[Order Subtotals] OS ON (OS.OrderID = O.OrderID)
+INNER JOIN dbo.[Order Subtotals] OS ON (OS.OrderID = O.OrderID);
+
 /* 13. Inserte un cliente completando todos los campos de la tabla */
 INSERT INTO dbo.Customers ( 
 CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode,
@@ -73,13 +83,16 @@ Country, Phone, Fax
 'San Nicolás', 'MTY', '66451', 'Mexico', '(81) 8329-4001',
 '83294045');
 SELECT * FROM dbo.Customers C WHERE C.CustomerID = 'UANL';
+
 /* 14. Actualice el nombre del nuevo cliente pasando el número de identificación obtenido en el punto 13 */
 UPDATE dbo.Customers SET CompanyName = 'FCFM.LSTI'
 WHERE CustomerID = 'UANL';
 SELECT * FROM dbo.Customers C WHERE C.CustomerID = 'UANL';
+
 /* 15. Eliminar el cliente insertado en el punto 13 */
 DELETE FROM dbo.Customers WHERE CustomerID = 'UANL';
 SELECT * FROM dbo.Customers C WHERE C.CustomerID = 'UANL';
+
 /* 16. Seleccione los productos cuyos pedidos (incluidos sus clientes) se enviaron a Francia */
 SELECT P.ProductName, O.OrderID, C.CompanyName FROM dbo.Orders O 
 INNER JOIN dbo.Customers C ON (C.CustomerID = O.CustomerID)
