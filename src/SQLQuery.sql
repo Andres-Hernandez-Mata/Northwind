5364c4c8a8c988a850296e83431482053f0d7b35
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-08-18 20:29:46 -0500

    Seleccione el nombre de la empresa para todos los clientes con una dirección en la ciudad 'Buenos Aires' y 'Madrid'

--- src/SQLQuery.sql
+++ src/SQLQuery.sql
@@ -1,15 +1,6 @@
 Use Northwind;
 
 /* 1. Seleccione cuántos productos de la categoría de producto "Marisco" y "Productos lácteos" se han vendido a una dirección en "Alemania". */
-SELECT P.CategoryID, P.ProductName, C.CategoryName, O.ShipCountry, SUM(P.CategoryID) AS TotalEnvios 
-FROM dbo.Products P
-INNER JOIN dbo.Categories C ON (C.CategoryID = P.CategoryID)
-INNER JOIN dbo.[Order Details] OD ON (OD.ProductID = P.ProductID)
-INNER JOIN dbo.Orders O ON (O.OrderID = OD.OrderID)
-WHERE C.CategoryName IN ('Seafood', 'Dairy Products') AND O.ShipCountry = 'Germany'
-GROUP BY P.ProductName, C.CategoryName, O.ShipCountry, P.CategoryID
-ORDER BY P.CategoryID;
-
 SELECT P.ProductName, C.CategoryName, O.ShipCountry, COUNT(P.ProductID) AS TotalEnvios
 FROM dbo.Orders O
 INNER JOIN dbo.[Order Details] OD ON (OD.OrderID = O.OrderID)
@@ -19,7 +10,10 @@ WHERE C.CategoryName IN ('Seafood', 'Dairy Products') AND O.ShipCountry = 'Germa
 GROUP BY P.ProductName, C.CategoryName, O.ShipCountry, P.ProductID, C.CategoryID
 ORDER BY C.CategoryID;
 
+/* 2. Seleccione el nombre de la empresa para todos los clientes con una dirección en la ciudad 'Buenos Aires' y 'Madrid'. */
+SELECT CompanyName, City FROM dbo.Customers 
+WHERE City IN ('Buenos Aires', 'Madrid')
+ORDER BY City DESC;
 
-/*Camembert Pierrot 10*/
-SELECT * FROM dbo.Orders;
-SELECT * FROM dbo.Categories;
+/* 3. Seleccione el uso del valor del subtotal (precio unitario y cantidad) en el detalle del pedido de venta para listar los pedidos del más grande al más pequeño. Para cada pedido mostrar el Nombre de la Empresa y el SubTotal y el peso total del pedido. */
+
