55db8c4294ecf14044134c5bb8d14155493e1181
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-08-25 19:36:35 -0500

    CREATE PROCEDURE SPCustomers

--- src/SPCustomers.sql
+++ src/SPCustomers.sql
@@ -12,56 +12,74 @@ CREATE PROCEDURE SPCustomers (
 		@Country		VARCHAR(15) = NULL,
 		@Phone			VARCHAR(24) = NULL,
 		@Fax			VARCHAR(24) = NULL,
-		@Accion			CHAR(15)
+		@Accion			CHAR(15)    = NULL
 	)
 AS
 BEGIN
 	
 	IF @Accion = 'SELECT'
 	BEGIN
-		SELECT *
-		FROM Customers
+		IF EXISTS (SELECT * FROM Customers WHERE CustomerID = @CustomerID)
+			SELECT *
+			FROM Customers
+			WHERE CustomerID = @CustomerID
+		ELSE
+			SELECT CONCAT('El cliente ', @CustomerID, ' no se encuentra en la base de datos') AS Mensaje
 	END
 
 	ELSE IF @Accion = 'INSERT'
 	BEGIN
-		INSERT INTO Customers
-		VALUES (
-			@CustomerID,
-			@CompanyName,
-			@ContactName,	
-			@ContactTitle,
-			@Address,
-			@City,
-			@Region,
-			@PostalCode,
-			@Country,
-			@Phone,
-			@Fax
-		)
+		IF NOT EXISTS (SELECT * FROM Customers WHERE CustomerID = @CustomerID)
+			INSERT INTO Customers
+			VALUES (
+				@CustomerID,
+				@CompanyName,
+				@ContactName,	
+				@ContactTitle,
+				@Address,
+				@City,
+				@Region,
+				@PostalCode,
+				@Country,
+				@Phone,
+				@Fax
+				)
+		ELSE
+			SELECT CONCAT('El cliente ', @CustomerID, ' ya existe en la base de datos') AS Mensaje
 	END
 
 	ELSE IF @Accion = 'UPDATE'
 	BEGIN
-		UPDATE Customers
-		SET CustomerID   = @CustomerID,
-			CompanyName  = @CompanyName,
-			ContactName  = @ContactName,	
-			ContactTitle = @ContactTitle,
-			Address      = @Address,
-			City         = @City,
-			Region       = @Region,
-			PostalCode   = @PostalCode,
-			Country      = @Country,
-			Phone        = @Phone,
-			Fax          = @Fax
+		IF EXISTS (SELECT * FROM Customers WHERE CustomerID = @CustomerID)
+			UPDATE Customers			
+			SET	CompanyName  = @CompanyName,
+				ContactName  = @ContactName,	
+				ContactTitle = @ContactTitle,
+				Address      = @Address,
+				City         = @City,
+				Region       = @Region,
+				PostalCode   = @PostalCode,
+				Country      = @Country,
+				Phone        = @Phone,
+				Fax          = @Fax
+			WHERE CustomerID = @CustomerID
+		ELSE
+			SELECT CONCAT('El cliente ', @CustomerID, ' no se encuentra en la base de datos') AS Mensaje
 	END
 
 	ELSE IF @Accion = 'DELETE'
 	BEGIN
-		DELETE
-		FROM Customers
-		WHERE CustomerID = @CustomerID
+		IF EXISTS (SELECT * FROM Customers WHERE CustomerID = @CustomerID)
+			DELETE
+			FROM Customers
+			WHERE CustomerID = @CustomerID
+		ELSE
+			SELECT CONCAT('El cliente ', @CustomerID, ' no se encuentra en la base de datos') AS Mensaje
+	END
+
+	ELSE IF @Accion = ''
+	BEGIN
+		SELECT 'Favor de seleccionar una de las siguientes acciones SELECT, INSERT, UPDATE O DELETE' AS Mensaje
 	END
 
 END
