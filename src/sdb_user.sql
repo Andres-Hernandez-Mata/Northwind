aa0ce6e8ccb3028da000558f380b3a8e2022dec4
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-26 13:54:37 -0500

    Usuario sdb_user

---
+++ src/sdb_user.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA class_sbd
+
+	CREATE TABLE Prueba(
+		id INT
+	)
+
+
+DENY INSERT On SCHEMA::dbo To sdb_user
+
+
+	UPDATE dbo.Customers SET Address = 'Obere Str. 57' WHERE CustomerID = 'ALFKI'
+
