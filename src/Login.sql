582bd797dc5f2d17ee6da8c7356f3ad52bcb1e54
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-26 13:54:17 -0500

    Login

---
+++ src/Login.sql
@@ -0,0 +1,10 @@
+	
+	DENY INSERT On SCHEMA::[dbo] To sdb_user
+	DENY UPDATE On SCHEMA::[dbo] To sdb_user
+	DENY DELETE On SCHEMA::[dbo] To sdb_user
+
+
+UPDATE dbo.Customers SET Address = 'Obere Str. 57' WHERE CustomerID = 'ALFKI'
+
+
+
