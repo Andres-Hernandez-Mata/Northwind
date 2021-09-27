890303cd6f8b3752845276d913f0730fb27595ad
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-26 19:48:11 -0500

    Usuario vendedor

---
+++ src/vendedor.sql
@@ -0,0 +1,18 @@
+SELECT * INTO ventas.Orders FROM dbo.Orders
+
+SELECT * INTO ventas.[Order Details] FROM dbo.[Order Details]
+
+SELECT * INTO ventas.Customers FROM dbo.Customers
+
+
+SELECT * FROM ventas.Orders
+SELECT * FROM ventas.[Order Details]
+SELECT * FROM ventas.Customers
+
+	INSERT INTO dbo.Employees (LastName, FirstName) VALUES ('Hernandez', 'Andres')
+
+SELECT * FROM ventas.Customers
+
+	INSERT INTO Customers (CustomerID, CompanyName) VALUES ('HDZM', 'Hernandez')
+
+
