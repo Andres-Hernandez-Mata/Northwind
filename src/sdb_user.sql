6d17af585a2fbea1cac006c45fdeb274a686620f
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-29 19:56:58 -0500

    sdb_user

--- src/sdb_user.sql
+++ src/sdb_user.sql
@@ -1,12 +1,30 @@
 CREATE SCHEMA class_sbd
 
-	CREATE TABLE Prueba(
-		id INT
-	)
+CREATE TABLE Prueba(
+	id INT
+)
 
 
 DENY INSERT On SCHEMA::dbo To sdb_user
 
 
-	UPDATE dbo.Customers SET Address = 'Obere Str. 57' WHERE CustomerID = 'ALFKI'
+UPDATE dbo.Customers SET Address = 'Obere Str. 57' WHERE CustomerID = 'ALFKI'
 
+
+
+
+SELECT * FROM SYS.sysprocesses
+
+SELECT hostname AS Servidor, 
+login_time AS 'Fecha Hora',
+loginame AS Usuario,
+COUNT(dbid) AS 'Numero de Conexiones'
+FROM SYS.sysprocesses
+GROUP BY dbid, login_time, hostname, loginame
+ORDER BY COUNT( dbid ) DESC
+
+
+KILL 66
+
+
+sp_who2
