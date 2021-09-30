475d72d0da059ed784240cb2c1801fb1d29dac23
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-09-29 19:56:42 -0500

    Administrador

--- src/administrador.sql
+++ src/administrador.sql
@@ -1,26 +1,26 @@
 
 
-	INSERT INTO ventas.Customers (CustomerID, CompanyName) VALUES ('HDZM', 'Hernandez')
+INSERT INTO ventas.Customers (CustomerID, CompanyName) VALUES ('HDZM', 'Hernandez')
 
-	INSERT INTO administracion.Products (ProductName, Discontinued) VALUES ('Teclado', 1)
+INSERT INTO administracion.Products (ProductName, Discontinued) VALUES ('Teclado', 1)
 
-	SELECT * FROM ventas.Customers
+SELECT * FROM ventas.Customers
 
-	SELECT * FROM administracion.Products WHERE ProductName = 'Teclado'
+SELECT * FROM administracion.Products WHERE ProductName = 'Teclado'
 
 
-	INSERT INTO administracion.Suppliers (CompanyName) VALUES ('LSTI')
+INSERT INTO administracion.Suppliers (CompanyName) VALUES ('LSTI')
 
-	SELECT * FROM Suppliers WHERE CompanyName = 'LSTI'
+SELECT * FROM Suppliers WHERE CompanyName = 'LSTI'
 	
 
 	
 
-	UPDATE ventas.Orders SET ShipRegion = 'JK' WHERE OrderID = 10248
+UPDATE ventas.Orders SET ShipRegion = 'JK' WHERE OrderID = 10248
 
-	SELECT * FROM ventas.Orders WHERE OrderID = 10248
+SELECT * FROM ventas.Orders WHERE OrderID = 10248
 
 	
-	UPDATE ventas.[Order Details] SET Discount = 0.15 WHERE OrderID = 10248
+UPDATE ventas.[Order Details] SET Discount = 0.15 WHERE OrderID = 10248
 
-	SELECT * FROM ventas.[Order Details] WHERE OrderID = 10248
+SELECT * FROM ventas.[Order Details] WHERE OrderID = 10248
