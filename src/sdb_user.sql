43b2f9d08fd3b0991307378bcfec84567421ec6a
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-10-19 23:49:44 -0500

    Procedimientos

--- src/sdb_user.sql
+++ src/sdb_user.sql
@@ -31,3 +31,20 @@ SELECT * FROM Membership;
 
 	EXEC SPEncriptacion
 
+	EXEC OcultarInformacion;
+
+	EXEC VerInformacion;
+
+	GRANT UNMASK
+
+	SELECT * FROM lsbd_EncryMask;
+
+	DENY UNMASK TO sdb_user
+
+	INSERT INTO lsbd_EncryMask(infoEncryNombre, infoMaskDireccion, infoMaskTelefono,
+		infoMaskEmail) VALUES 
+		(ENCRYPTBYPASSPHRASE('LSTI', 'Andres'), 'CALLE AGUSTIN LARA NO. 69-B', '8181378772', 'andres@gmail.com'), 
+		(ENCRYPTBYPASSPHRASE('LSTI', 'Juan'), 'AV. INDEPENDENCIA NO. 241', '8281984162', 'juan@hotmail.com'),
+		(ENCRYPTBYPASSPHRASE('LSTI', 'Angel'), 'CALLE MATAMOROS NO. 40', '8681883164', 'angel@gmail.com'),
+		(ENCRYPTBYPASSPHRASE('LSTI', 'Luis'), 'C. GUADALUPE VICTORIA NO 373', '8486483149', 'luis@hotmail.com'),
+		(ENCRYPTBYPASSPHRASE('LSTI', 'Missael'), 'AV. MANCILLA ESQ. ALDAMA S/N', '8746389151', 'missael@gmail.com');
