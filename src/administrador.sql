2700be6862aa72b74ea806b6f485ba9a3d93f56a
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-11-26 20:10:25 -0600

    administrador

--- src/administrador.sql
+++ src/administrador.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO ventas.Customers (CustomerID, CompanyName) VALUES ('HDZM', 'Hernandez')
 
 INSERT INTO administracion.Products (ProductName, Discontinued) VALUES ('Teclado', 1)
@@ -12,9 +10,7 @@ SELECT * FROM administracion.Products WHERE ProductName = 'Teclado'
 INSERT INTO administracion.Suppliers (CompanyName) VALUES ('LSTI')
 
 SELECT * FROM Suppliers WHERE CompanyName = 'LSTI'
-	
-
-	
+		
 
 UPDATE ventas.Orders SET ShipRegion = 'JK' WHERE OrderID = 10248
 
@@ -24,3 +20,4 @@ SELECT * FROM ventas.Orders WHERE OrderID = 10248
 UPDATE ventas.[Order Details] SET Discount = 0.15 WHERE OrderID = 10248
 
 SELECT * FROM ventas.[Order Details] WHERE OrderID = 10248
+
